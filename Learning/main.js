570e729b2c5c336b0b207dc4539127238bb94767
Kyoteki <victor.bordeaux78@gmail.com>
2023-09-26 17:44:56 +0200

    Exercices Algo JS

---
+++ Learning/main.js
@@ -0,0 +1,14 @@
+alert("Bienvenue, jeune voyageur")
+
+console.log("Tu as maintenant accès à tes nouveaux pouvoirs")
+
+
+const student1Age = 20
+const student2Age = 19
+
+if (student1Age > student2Age) {
+	console.log ("l'édudiant 1 est le plus vieux")
+} 
+else {
+	console.log ("lédudiant 2 est le plus vieux")
+}
