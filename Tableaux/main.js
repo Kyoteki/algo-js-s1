e45e3f5672833306c7ab4df296479c17bbaa23f0
Kyoteki <victor.bordeaux78@gmail.com>
2023-09-27 17:52:41 +0200

    algo

--- Tableaux/main.js
+++ Tableaux/main.js
@@ -5,3 +5,13 @@ names.push("Vincent", "Paul", "Arthur")
 let comments("va à la pèche")
 
 console.log(names)
+
+
+let User = {
+    nom : "Saber",
+    prénom : "Beat",
+    naissance : "01/04/2018",
+    passion : "Musique",
+}
+
+console.log(User)
