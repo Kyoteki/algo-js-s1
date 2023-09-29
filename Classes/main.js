019a3992b0b39f6e2d7c6fec890cba40d49359ed
Kyoteki <victor.bordeaux78@gmail.com>
2023-09-29 14:07:38 +0200

    algo

--- Classes/main.js
+++ Classes/main.js
@@ -5,18 +5,16 @@ class Pokemon {
         this.attack=attack
         this.defense=defense
         this.luck=luck
- }
+    }
     AttackPokemon (Opponent, damages = this.attack - Opponent.defense) {
         if (damages <= 0){
             damages=1
+            Opponent.hp = Opponent.hp - damages
         }
-        Opponent.hp = Opponent.hp - damages
     }
-
 }
-
 let Garchomp = new Pokemon("Garchomp", 108, 130, 95, 50)
-let Greninja = new Pokemon("Greninja", 72, 95, 67, 50)
+let Greninja = new Pokemon("Greninja", 72, 95, 67, 70)
 
 console.log(Garchomp.name, Garchomp.hp, "HP")
 console.log(Greninja.name, Greninja.hp, "HP")
@@ -28,7 +26,7 @@ while (Garchomp.hp >= 1 || Greninja.hp >=1) {
         console.log(Garchomp.name, Garchomp.hp, "hp")
     }
     else {
-        console.log(Greninja.name, "win")
+        console.log(Garchomp.name, "is KO.", Greninja.name, "win")
         break
     }
     if (Greninja.hp >0){
@@ -37,7 +35,7 @@ while (Garchomp.hp >= 1 || Greninja.hp >=1) {
         console.log(Greninja.name, Greninja.hp, "hp")
     }
     else {
-        console.log(Garchomp.name, "win")
+        console.log(Greninja.name, "is KO.", Garchomp.name, "win")
         break
     }
 }
